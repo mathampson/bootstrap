8cbcca376a834d20b7af4db3d42c6716dba2fb2f
hampsonm <m.hampson@elsevier.com>
2018-09-06 12:05:54 +0100

    CSS asset path fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './_assets/main.css';
+import './_assets/css/main.css';
 
 class App extends Component {
   render() {
